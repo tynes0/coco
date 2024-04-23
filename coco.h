ae3c8a693d6164d1cdea2d7f8c668455c8145b05
Cihan Bilgihan <94874242+tynes0@users.noreply.github.com>
2024-04-24 02:57:10 +0300

    Add attribution and link to original Coco library in coco.h

--- coco.h
+++ coco.h
@@ -1,3 +1,17 @@
+/*
+ * This file is part of the Coco library, originally created by Tynes0.
+ * For the latest version and updates, please visit the official Coco GitHub repository:
+ * https://github.com/tynes0/coco
+ *
+ * Coco is a comprehensive library for time measurements, providing JSON generation for use in the tracing part of internet browsers, logging data to a given file, logging data to the console, etc..
+ * It is released under the Apache License 2.0 license. See the LICENSE file in the root of the Coco repository
+ * or visit the above GitHub link for more details.
+ *
+ * For contributions, bug reports, or other inquiries, feel free to contact the author:
+ * - GitHub: https://github.com/tynes0
+ * - Email: cihanbilgihan@gmail.com
+ */
+
 #pragma once
 
 #include <chrono>
